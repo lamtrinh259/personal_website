7aee8093cf03c064e2d0d647dbb66689c604a2f9
Lam Trinh <lam.trinh@global.t-bird.edu>
2023-08-12 12:20:49 +0900

    The blog page is still not rendering properly, test deployed on dev

--- app/blog/[slug]/page.tsx
+++ app/blog/[slug]/page.tsx
@@ -1,31 +1,30 @@
 import fs from "fs";
-// import React from "react";
 import Markdown from "markdown-to-jsx";
 import matter from "gray-matter";
 import getPostMetadata from "../../../components/getPostMetadata";
 
 const getPostContent = (slug: string) => {
   const folder = "posts";
-  const file = '${folder}/${slug}.md';
+  const file = `${folder}/${slug}.md`;
   const content = fs.readFileSync(file, "utf8");
   const matterResult = matter(content);
-  return matterResult; // Should only the return the contents of the post, not the metadata
+  return matterResult.content; // Return only the contents of the post
 };
 
 export const generateStaticParams = async () => {
   const posts = getPostMetadata();
   return posts.map((post) => ({
     slug: post.slug,
-  }))
+  }));
 };
 
-const PostPage = (props:any) => {
+const PostPage = (props: any) => {
   const slug = props.params.slug;
-  const post = getPostContent(slug);
+  const postContent = getPostContent(slug);
   return (
     <div>
-      <h1>{post.data.Title}{slug}</h1>
-      <Markdown>{post.content}</Markdown>
+      <h1>{post.data.Title}</h1>
+      <Markdown>{postContent}</Markdown>
     </div>
   );
 };
