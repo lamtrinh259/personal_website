c594e2d021745dd847b79aeb93016ba17ea6f362
Lam Trinh <lam.trinh@global.t-bird.edu>
2024-03-04 12:59:57 +0900

    Added a podcast section below the Hero section where visitors can play the latest episode on my website

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,7 @@
 import AboutSection from "@/components/AboutSection"
 import HeroSection from "@/components/HeroSection"
 import ProjectsSection from "@/components/ProjectsSection"
+import PodcastSection from "@/components/PodcastSection"
 
 // This is the main page of the app
 
@@ -8,6 +9,7 @@ export default function Home() {
   return (
     <main className= "mx-auto max-w-3xl px-4 sn:px-6 md:max-w-5xl" >
     <HeroSection />
+    < PodcastSection />
     < AboutSection />
     <ProjectsSection />
       </main>
